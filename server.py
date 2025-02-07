c62dfcb36147b57b8d0c741ce773ace0c5117936
parth-royale <chadsaragon@googlemail.com>
2025-02-07 06:46:14 +0530

    Update server.py

--- server.py
+++ server.py
@@ -195,8 +195,9 @@ def main():
 
     threading.Thread(target=ws_client_connect, daemon=True).start()
     threading.Thread(target=check_and_flush_deque, daemon=True).start()
-
-    app.run(debug=True, port=8000)
+    
+    port = int(os.environ.get("PORT", 8000))  # Get port from environment or default to 8000
+    app.run(debug=False, host="0.0.0.0", port=port)  # Bind to 0.0.0.0 for Heroku
 
 
 if __name__ == "__main__":
